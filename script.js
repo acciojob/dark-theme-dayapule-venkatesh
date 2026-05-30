3bfeb93b16a92c45d3d2d51e0d28ea73bd0c52ed
Dayapule Venkatesh <106541414+dayapule-venkatesh@users.noreply.github.com>
2026-05-30 22:07:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+let btn=document.querySelector("#swap");
+let container=document.querySelector("#app")
+let current=true
+btn.addEventListener("click", (e)=>{
+	if(current){
+		container.style.backgroundColor="black"
+		current=false
+	}
+	else{
+		container.style.backgroundColor="white";
+		current=true;
+	}
+})
 
